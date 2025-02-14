fcad10ef5d5a5eee9ba4420661d7ce021e1cd785
Sara <156946666+sara-1129@users.noreply.github.com>
2025-02-14 13:03:49 +0530

    Time: 7 ms (16.12%) | Memory: 8.5 MB (67.95%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0)
- {
-    return false;
- } 
-    int temp = x;
-    int ans = 0;
-    if (x == 0)
-    {
+        if (x < 0)
+        {
+            return false;
+        }
+        if (x==0)
+        {
+            return true;
+        }
+        int temp = x;
+        int rev = 0;
+        while(temp > 0)
+        {
+            int digit = temp % 10;
+            if (rev < pow(-2,31)/10 || rev > (pow(2,31)-1)/10)
+            {
+               return 0;
+            }   
+             rev = rev * 10 +digit;
+                temp = temp /10; 
+        }
+       if (rev == x)
+       {
         return true;
-    }
-
-    while (temp != 0)
-    {
-        int digit = temp % 10;
-        if(ans>(INT_MAX)/10 || ans<(INT_MIN)/10){
-                return false;
-            }
-        ans = ans * 10 + digit;
-        temp = temp / 10;
-    }
-    if (ans == x)
-    {
-        return true;
-    }
-    return false;
- 
-
+       } 
+       return false;
     }
 };
