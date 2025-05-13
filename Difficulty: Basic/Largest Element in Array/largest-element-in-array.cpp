c8fac64a89325d21b54d5c2e25557facff393ef3
Sara <156946666+sara-1129@users.noreply.github.com>
2025-05-13 12:33:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
@@ -16,15 +16,8 @@ class Solution {
     int largest(vector<int> &arr) {
         // code here
         int n = arr.size();
-        int max = INT_MIN;
-        for(int i = 0; i < n; i++)
-        {
-            if(arr[i] > max)
-            {
-                max = arr[i];
-            }
-        }
-        return max;
+        sort(arr.begin(),arr.end());
+        return arr[n-1];
     }
 };
 
