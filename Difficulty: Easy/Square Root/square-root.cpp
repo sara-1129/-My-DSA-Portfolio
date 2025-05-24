3be2b16243f157ae7a0939701db57cd1998e9278
Sara <156946666+sara-1129@users.noreply.github.com>
2025-05-24 13:18:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.cpp
@@ -0,0 +1,9 @@
+// Function to find square root
+// x: element to find square root
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        // Your code goes here
+        return sqrt(n);
+    }
+};
