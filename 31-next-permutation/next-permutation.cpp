3d8ddaa7da249190dc0abf6c5bdf22f41bbe95d6
Sara <156946666+sara-1129@users.noreply.github.com>
2025-06-17 16:34:12 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (92.49%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,26 +2,29 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1)return;
-        int breakpoint = -1;
-        int nextGreatElement;
-        for(int i = n-2; i >= 0; i--) {
-            if(nums[i] < nums[i + 1]) {
-                breakpoint = i;
-                break;
-            }
+    if( n == 1) return;
+    int breakPt = -1;
+    int nxtGreatEle;
+    for(int i = n - 2; i >= 0; i--) {
+        if(nums[i] < nums[i + 1]) {
+            breakPt = i;
+            break;
         }
-        if(breakpoint == -1) {
-            reverse(nums.begin(),nums.end());
-            return;
+    }
+
+    if(breakPt == -1) {
+        reverse(nums.begin(),nums.end());
+        return;
+    }
+    for(int i = n - 1; i >= 0; i--) {
+        if(nums[i] > nums[breakPt]) {
+            nxtGreatEle = i;
+            break;
         }
-        for(int i = n-1; i >= 0; i--) {
-            if(nums[i] > nums[breakpoint]) {
-                nextGreatElement = i;
-                break;
-            }
-        }
-        swap(nums[nextGreatElement],nums[breakpoint]);
-        sort(nums.begin() + breakpoint + 1, nums.end());
+    }
+    swap(nums[breakPt],nums[nxtGreatEle]);
+    sort(nums.begin() + breakPt + 1,nums.end());
+
+    
     }
 };
