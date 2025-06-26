c58417f586dff38442b1cc200539fa2a05e3de91
Sara <156946666+sara-1129@users.noreply.github.com>
2025-06-26 12:09:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate Array/rotate-array.cpp
+++ Difficulty: Medium/Rotate Array/rotate-array.cpp
@@ -1,80 +1,22 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
 
     // Function to rotate an array by d elements in counter-clockwise direction.
     void rotateArr(vector<int>& arr, int d) {
         // code here
-        int n = arr.size();
-        if(d==0 ||n==1)
-        {
-            return;
-        }
-        
-        vector <int> temp;
-        d  = d%n;
+        int n  = arr.size();
+        vector<int> temp;
+        d = d % n;
         for(int i = d; i < n; i++)
         {
             temp.push_back(arr[i]);
         }
-        for(int i = 0; i < d; i++)
-        {
+        for(int i = 0; i < d; i++) {
             temp.push_back(arr[i]);
         }
-        for(int i = 0; i < n; i++)
-        {
+        
+        for(int i = 0; i < n; i++) {
             arr[i] = temp[i];
         }
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int d;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        d = crr[0];
-        int n = arr.size();
-        Solution ob;
-        // calling rotateArr() function
-        ob.rotateArr(arr, d);
-
-        // printing the elements of the array
-        for (int i = 0; i < n; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
