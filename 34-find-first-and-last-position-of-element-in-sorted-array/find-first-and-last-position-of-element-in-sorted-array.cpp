15679b646497b73965ff4ce10b8876e9494b57fe
Sara <156946666+sara-1129@users.noreply.github.com>
2025-07-01 18:49:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (52.54%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,20 +1,56 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+    int firstOccurence(vector<int>& nums, int target, int low, int high) {
         int first = -1;
-        int last = -1;
-        int n = nums.size();
-        for(int i = 0; i < n; i++)
+        while(high >= low)
         {
-            if(nums[i] == target)
+            int mid = low + (high - low)/2;
+            if(nums[mid] > target)
             {
-                if(first == -1)
-                {
-                    first = i;
-                }
-                last = i;
+                high = mid - 1;
+            }
+            else if(nums[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                first = mid;
+                high = mid - 1;
             }
         }
+        return first;
+    }
+    int lastOccurence(vector<int>& nums, int target, int low, int high) {
+        int last = -1;
+         while(high >= low)
+        {
+            int mid = low + (high - low)/2;
+            if(nums[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else if(nums[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                last = mid;
+                low = mid + 1;
+            }
+        }
+        return last;
+    }
+    
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        int first = firstOccurence(nums,target,low,high);
+        int last = lastOccurence(nums,target,low,high);
+
         return {first, last};
     }
 };
