8a00efcb3c14eb2b2f470f88a0ea3a78c0e321ad
Sara <156946666+sara-1129@users.noreply.github.com>
2025-07-10 18:38:53 +0530

    Time: 2 ms (44.98%) | Memory: 10.5 MB (67.89%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -21,7 +21,7 @@ public:
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<vector<int>> ans = {};
+        vector<vector<int>> ans;
         vector<int> temp;
         subsets(nums, temp, ans, 0, 0);
         return ans;
