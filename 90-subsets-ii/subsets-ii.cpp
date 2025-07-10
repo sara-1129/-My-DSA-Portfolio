d8b3389b08c49a61dac8d8f4a3fc9b8fa9ac9a99
Sara <156946666+sara-1129@users.noreply.github.com>
2025-07-10 18:28:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (85.28%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    void subsets(vector<int>& nums, vector<int>& temp, set<vector<int>>& ans, int idx)
+    void subsets(vector<int>& nums, vector<int>& temp, vector<vector<int>>& ans, int idx,int size)
     {
         int n = nums.size();
+        if((temp.size() == size))
+        {
+            ans.push_back(temp);
+        }
         if(idx >= n)
         {
-            ans.insert(temp);
             return;
         }
-        temp.push_back(nums[idx]);
-        subsets(nums, temp, ans, idx + 1);
-        temp.pop_back();
-        subsets(nums, temp, ans, idx + 1);
+        for(int i = idx; i < n; i++)
+        {
+            if(i > idx && nums[i] == nums[i - 1])continue;
+            temp.push_back(nums[i]);
+            subsets(nums, temp, ans, i + 1, size + 1);
+            temp.pop_back();
+        }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        set<vector<int>> ans;
+        vector<vector<int>> ans = {};
         vector<int> temp;
-        subsets(nums, temp, ans, 0);
-        vector<vector<int>> finalAns(ans.begin(), ans.end());
-        return finalAns;
+        subsets(nums, temp, ans, 0, 0);
+        return ans;
     }
 };
