b24761290cbbf0af3e323eb2b3ec4670131e729d
Sara <156946666+sara-1129@users.noreply.github.com>
2025-07-19 10:47:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check K-th Bit/check-kth-bit.cpp
+++ Difficulty: Easy/Check K-th Bit/check-kth-bit.cpp
@@ -1,30 +1,12 @@
 class Solution {
   public:
-    vector<int> convertBinary(int n )
-    {
-        vector<int> ans;
-        while(n > 0)
-        {
-            int digit = n % 2;
-            ans.push_back(digit);
-            n = n / 2;
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
-    }
     bool checkKthBit(int n, int k) {
         //  code here
-        vector <int> ans = convertBinary(n);
-        int m = ans.size();
-        if(k > m - 1)
-        {
-            return false;
-        }
-        if(ans[m - 1 - k] == 1)
+        int temp = n;
+        if((n >> k) & 1 != 0)
         {
             return true;
         }
         return false;
-        
     }
 };
