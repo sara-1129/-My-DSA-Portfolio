417f971de24dbe9cc84add14d42dd8ba8afd796b
Sara <156946666+sara-1129@users.noreply.github.com>
2025-07-26 15:45:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -1,58 +1,17 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // Please change the array in-place
-    void insertionSort(vector<int>& arr,int i = 0) {
+    void insertionSort(vector<int>& arr) {
         // code here
-        int n = arr.size();
-       
-        if (i == n-1)
+        int n  = arr.size();
+        for(int i = 0; i < n - 1; i++)
         {
-            return;
-        }
-            for (int j = i; j >= 0; j--)
+            for(int j = i + 1; j < n; j++)
             {
-                if (arr[j] > arr[j+1] )
+                if(arr[i] > arr[j] )
                 {
-                    swap(arr[j+1],arr[j]);
+                    swap(arr[i],arr[j]);
                 }
-                
             }
-            insertionSort(arr,i+1);
-           
-        
+        }
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        obj.insertionSort(arr);
-        for (int i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
