564d56e61b68a32380df59080344f26037285fb5
Sara <156946666+sara-1129@users.noreply.github.com>
2025-08-02 10:15:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Implement stack using array/implement-stack-using-array.cpp
+++ Difficulty: Basic/Implement stack using array/implement-stack-using-array.cpp
@@ -16,14 +16,15 @@ public:
 void MyStack ::push(int x) {
     // Your Code
     top++;
-    arr[top] = x; 
+    arr[top] = x;
 }
 
 // Function to remove an item from top of the stack.
 int MyStack ::pop() {
-    if(top == -1)return top;
+    // Your Code
+    if(top == -1)return -1;
     int ele = arr[top];
     top--;
     return ele;
-    // Your Code
+    
 }
