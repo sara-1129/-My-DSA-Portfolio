12de71349e45d1958a77f0dec82068a963dabc13
Sara <156946666+sara-1129@users.noreply.github.com>
2025-08-31 17:42:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (35.43%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();// gives number of rows 
-        vector<vector<int>> ans;
-        
-        for(int i = 0 ; i < n; i++) // outer loop is for columns
-        {   vector <int> temp;
-            for(int j = n - 1; j >= 0; j--) //Inner loop is for row traversal
+
+        for(int i = 0 ; i < n; i++) 
+        {  
+            for(int j = i + 1; j < n; j++) 
             {
-                temp.push_back(matrix[j][i]);
+                swap(matrix[i][j], matrix[j][i]);
             }
-            ans.push_back(temp);
         }
         for(int i = 0 ; i < n; i++)
         {   
-             matrix[i] = ans[i];
+             reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
