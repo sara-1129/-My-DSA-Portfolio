fa0c1df2d0a72b7939925aa9c49212c136d9b50d
Sara <156946666+sara-1129@users.noreply.github.com>
2025-09-22 22:27:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -1,82 +1,40 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
+    void merge(vector<int>& arr, int low, int mid, int high) {
+        vector<int> temp;
+        int left = low;
+        int right = mid + 1;
+        while(left <= mid && right <= high) {
+            if(arr[left] <= arr[right]) {
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else {
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+        while(left <= mid) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        while(right <= high) {
+            temp.push_back(arr[right]);
+            right++;
+        }
+        int j = 0;
+        for(int i = low; i <= high; i++) {
+            arr[i] = temp[j];
+            j++;
+        }
+    }
     void mergeSort(vector<int>& arr, int l, int r) {
         // code here
-        vector <int> temp;
-        if (l>=r)
-        {
-            return;
-        }
-        int mid =(l+r)/2;
-        int low = l;
-         int high = mid + 1;
-         mergeSort(arr,l,mid);
-        mergeSort(arr,mid+1,r);
-        while(low <= mid && high <= r)
-        {
-           if(arr[low] <= arr[high])
-           {
-               temp.push_back(arr[low]);
-               low++;
-           }
-           else 
-           {
-              temp.push_back(arr[high]);
-               high++; 
-           }
-        }
-        while(low <= mid)
-        {
-            temp.push_back(arr[low]);
-               low++;
-        }
-        while(high <= r)
-        {
-            temp.push_back(arr[high]);
-               high++; 
-        }
+        if (l >= r)return;
         
-        
-        
-        for(int i = l; i<= r; i++)
-        {
-            arr[i] = temp[i-l];
-        }
-        
-        
+        int mid = (l + r)/2;
+        mergeSort(arr, l , mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        obj.mergeSort(arr, 0, arr.size() - 1);
-        for (int i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
