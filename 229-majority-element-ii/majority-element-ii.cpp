05d68461d0fbe95133e9ed64a1ed7b929f2f68ee
Sara <156946666+sara-1129@users.noreply.github.com>
2025-09-26 08:11:56 +0530

    Time: 462 ms (5.55%) | Memory: 23.6 MB (81.61%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        set<int> temp;
         vector<int> ans;
         for(int i = 0; i < n; i++) {
-            int cnt = 1;
+           if(ans.size() == 0 || ans[0] != nums[i]) {
+             int cnt = 1;
             for(int j = i + 1; j < n; j++) {
                 if(nums[i] == nums[j]) {
                     cnt++;
                 }
             }
-            if(cnt > n/3)temp.insert(nums[i]);
+            if(cnt > n/3)ans.push_back(nums[i]);
+            if(ans.size() == 2)break;
+           }
         }
-        for(auto it :temp) {
-            ans.push_back(it);
-        }
+        
         return ans;
     }
 };
