d25572f00b9b67356518a369dedcfb488f6fd7ef
Sara <156946666+sara-1129@users.noreply.github.com>
2026-03-23 21:42:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.cpp
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -3,12 +3,10 @@ class Solution {
     int missingNum(vector<int>& arr) {
         // code here
         int n = arr.size() + 1;
-        int xor1 = 0;
+        int xor1 = n^(n-1);
         int xor2 = 0;
-        for(int i = 1; i <= n; i++) {
-            xor1 ^= i;
-        }
         for(int i = 0; i < n - 1; i++) {
+            xor1 ^= i;
             xor2 ^= arr[i];
         }
         return xor1^xor2;
